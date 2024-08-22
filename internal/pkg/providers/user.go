b4a1a9b5f11769a9ed3818e8231550e8b91664a6
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2024-08-22 17:28:36 -0300

    fix/structure

---
+++ internal/pkg/providers/user.go
@@ -0,0 +1,43 @@
+package providers
+
+import (
+	"errors"
+	"texting-app/internal/pkg/models"
+	"texting-app/internal/pkg/utils"
+)
+
+type userStore interface {
+	GetUser(username string) (*models.User, error)
+}
+
+type UserProvider struct {
+	store userStore
+}
+
+func NewUserProvider(s userStore) *UserProvider {
+	return &UserProvider{
+		store: s,
+	}
+}
+
+func (s *UserProvider) AuthUser(username string, password string) (*models.User, error) {
+	user, err := s.store.GetUser(username)
+	if err != nil {
+		return nil, err
+	}
+
+	cryptMngr := utils.NewCryptoManager(
+		"vault/public-key.pem",
+		"vault/private-key.pem",
+	)
+	decrypted, err := cryptMngr.Decrypt(user.Password, username)
+	if err != nil {
+		return nil, err
+	}
+
+	if password != string(decrypted) {
+		return nil, errors.New("unauthorized")
+	}
+
+	return user, nil
+}
--- internal/pkg/store/store.go
+++ internal/pkg/store/store.go
@@ -1,4 +1,4 @@
-package repository
+package store 
 
 import (
 	"database/sql"
@@ -12,17 +12,32 @@ const (
 	CONNSTRING = ""
 )
 
-type provider struct {
+type store struct {
 	db *sql.DB
 }
 
-func (p *provider) Close() error {
+func (p *store) Close() error {
+	if p.db == nil {
+		panic("Missing database connection")
+	}
+
 	return p.db.Close()
 }
 
-func (p *provider) GetUser(username string) (*models.User, error) {
-    query := ""
-    user := models.User{}
+func (p *store) GetUser(username string) (*models.User, error) {
+	if p.db == nil {
+		panic("Missing database connection")
+	}
+
+	query := `
+        SELECT TOP 1
+            USERNAME
+            , PASSWORD
+        FROM USERS (NOLOCK)
+        WHERE 1 = 1
+            AND USERNAME = ?;
+    `
+	user := models.User{}
 	err := p.db.QueryRow(query, username).Scan(&user.Username, &user.Password)
 	if err != nil {
 		return nil, fmt.Errorf("Error executing query:\n%s\n\t%v", query, err)
@@ -32,20 +47,20 @@ func (p *provider) GetUser(username string) (*models.User, error) {
 }
 
 var (
-	provInstance *provider
+	provInstance *store
 	once         sync.Once
 )
 
-func instantiate() (*provider, error) {
+func instantiate() (*store, error) {
 	db, err := sql.Open(DRIVER, CONNSTRING)
 	if err != nil {
 		return nil, fmt.Errorf("Error on database open: %v", err)
 	}
 
-	return &provider{db: db}, nil
+	return &store{db: db}, nil
 }
 
-func GetProvider() (*provider, error) {
+func GetStore() (*store, error) {
 	var err error
 	once.Do(func() {
 		provInstance, err = instantiate()
