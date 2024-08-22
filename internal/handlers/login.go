bdaf39de6f2273ed8b41d0d29ad76e2636caf9a7
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2024-08-22 00:55:15 -0300

    fix/get post switch

--- internal/handlers/login.go
+++ internal/handlers/login.go
@@ -1,20 +1,46 @@
 package handlers
 
 import (
-    "net/http"
+	"net/http"
+	"time"
 
-    "texting-app/templates"
+	"texting-app/internal/pkg/auth"
+	"texting-app/templates"
 )
 
 func Login(w http.ResponseWriter, r *http.Request) {
-    username := r.FormValue("username") 
-    password := r.FormValue("password") 
+	switch r.Method {
+	case http.MethodGet:
+		templ := templates.Login()
+		err := templ.Render(r.Context(), w)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+        return
+    case http.MethodPost:
+		username := r.FormValue("username")
+		password := r.FormValue("password")
 
-    token, err := 
+		user, err := auth.AuthenticateUser(username, password)
+		if err != nil {
+			if err.Error() == "unauthorized" {
+				http.Error(w, err.Error(), http.StatusUnauthorized)
+			} else {
+				http.Error(w, err.Error(), http.StatusInternalServerError)
+			}
+			return
+		}
 
-    templ := templates.Login()
-    err := templ.Render(r.Context(), w)
-    if err != nil {
-       http.Error(w, err.Error(), http.StatusInternalServerError) 
-    }
+		token := user.GetToken()
+		cookie := &http.Cookie{
+			Name:    "authenticationToken",
+			Value:   token,
+			Expires: time.Now().Add(time.Hour * 2),
+		}
+		http.SetCookie(w, cookie)
+		http.Redirect(w, r, "/home", http.StatusFound)
+        return
+	default:
+		return
+	}
 }
