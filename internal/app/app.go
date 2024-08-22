d85a56ca87ca4e86153f0892d4676d54f4a6364f
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2024-08-22 00:20:09 -0300

    ref/misc

--- internal/app/app.go
+++ internal/app/app.go
@@ -4,21 +4,8 @@ import (
 	"net/http"
 )
 
-func mapRoutes() *http.ServeMux {
-	mux := http.NewServeMux()
-	miscController := MiscController{}
-	wsController := WSController{}
-
-	mux.HandleFunc("/test", miscController.test)
-	mux.HandleFunc("/ws", wsController.handleConn)
-
-    go wsController.handleMessages()
-
-	return mux
-}
-
 func Run() error {
-	router := mapRoutes()
+	router := MapRoutes()
 	server := &http.Server{
 		Addr:    ":8080",
 		Handler: router,
--- internal/app/handlers.go
+++
@@ -1,9 +0,0 @@
-package app
-
-import "net/http"
-
-type MiscController struct{}
-
-func (self *MiscController) test(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Hello World!"))
-}
