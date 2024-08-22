4ff4fb0ec4a3cecc1173035fd2be6a5af0158c8b
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2024-08-22 17:31:36 -0300

    feat/encryption

--- internal/pkg/utils/encryption.go
+++ internal/pkg/utils/encryption.go
@@ -1,26 +1,80 @@
 package utils
 
+import (
+	"crypto/rand"
+	"crypto/rsa"
+	"crypto/sha256"
+	"crypto/x509"
+	"encoding/pem"
+	"os"
+)
+
 type cryptoMng struct {
-    publicKeyPath string
-    privateKeyPath string
+	pubKey  *rsa.PublicKey
+	privKey *rsa.PrivateKey
 }
 
-func (c *cryptoMng) ReadPublicKey(path string) error {
-    return nil
+func (c *cryptoMng) Encrypt(value string, factor string) ([]byte, error) {
+    hash := sha256.New()
+    hash.Write([]byte(factor))
+    ciphertext, err := rsa.EncryptOAEP(
+        hash,
+        rand.Reader,
+        c.pubKey,
+        []byte(value),
+        nil,
+    )
+    if err != nil {
+        return nil, err
+    }
+	return ciphertext, nil
 }
 
-func (c *cryptoMng) ReadPrivateKey(path string) error {
-    return nil
+func (c *cryptoMng) Decrypt(ciphertext string, factor string) ([]byte, error) {
+    hash := sha256.New()
+    hash.Write([]byte(factor))
+    plainText, err := rsa.DecryptOAEP(
+        hash,
+        rand.Reader,
+        c.privKey,
+        []byte(ciphertext),
+        nil,
+    )
+    if err != nil {
+        return nil, err
+    }
+	return plainText, nil
 }
 
-func (c *cryptoMng) Encrypt(value string, factor string) (*string, error) {
-    return nil, nil
-}
+// Returns a pointer to a cryptoMng instance.
+//
+// If there is a problem reading the files or parsing them into RSA keys,
+// calls panic() with the root error as argument
+func NewCryptoManager(pubKeyPath string, privKeyPath string) *cryptoMng {
+	pubKeyFile, err := os.ReadFile(pubKeyPath)
+	if err != nil {
+		panic(err)
+	}
 
-func (c *cryptoMng) Decrypt(ciphertext string, factor string) (*string, error) {
-    return nil, nil
-}
+	pubKeyPEM, _ := pem.Decode(pubKeyFile)
+	pubKey, err := x509.ParsePKCS1PublicKey(pubKeyPEM.Bytes)
+	if err != nil {
+		panic(err)
+	}
 
-func NewCryptoManager() *cryptoMng {
-    return &cryptoMng{}
+	privKeyFile, err := os.ReadFile(privKeyPath)
+	if err != nil {
+		panic(err)
+	}
+
+	privKeyPEM, _ := pem.Decode(privKeyFile)
+	privKey, err := x509.ParsePKCS1PrivateKey(privKeyPEM.Bytes)
+	if err != nil {
+		panic(err)
+	}
+
+	return &cryptoMng{
+		pubKey:  pubKey,
+		privKey: privKey,
+	}
 }
