678912368f9c18adf7024f42c309a0f06face04f
arthuralmeidadev <arthuralmeida.office.dev@gmail.com>
2024-08-28 20:27:16 -0300

    feat/files server, signup, chats, chat-msg-list

--- internal/app/route-map.go
+++ internal/app/route-map.go
@@ -2,15 +2,21 @@ package app
 
 import (
 	"net/http"
-	"texting-app/internal/handlers"
+	"texting-app/internal/api/handlers"
+	"texting-app/internal/api/middlewares"
 )
 
 func MapRoutes() *http.ServeMux {
 	mux := http.NewServeMux()
 	wsController := handlers.WSController{}
+	pubFilesHandler := http.FileServer(http.Dir("./static"))
 
+	mux.Handle("/public/",http.StripPrefix("/public/", pubFilesHandler))
 	mux.HandleFunc("/ws", wsController.HandleConn)
 	mux.HandleFunc("/login", handlers.Login)
+    mux.HandleFunc("/signup", handlers.Signup)
+	mux.HandleFunc("/chats", handlers.Chats)
+    mux.HandleFunc("/hx/chat-msg-list", middlewares.HTMXOnly(handlers.ChatMsgList))
 
 	go wsController.HandleMessages()
 
